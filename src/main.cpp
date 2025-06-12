f0cee7a9f2251b2fd337084a2ece59c71b88b744
kodenamekrak <kyelodge2005@gmail.com>
2025-06-12 15:44:19 +0100

    Handle uncaught exceptions and sigint

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,41 @@
-#include <arpa/inet.h>
-#include <netinet/in.h>
-#include <sys/socket.h>
-
 #include "device_tracker.hpp"
+#include "logger.hpp"
+
+#include <csignal>
+#include <atomic>
+#include <thread>
+
+static std::atomic<bool> quit = false;
+
+void signal_handler(int sig)
+{
+	quit = true;
+}
+
 int main(int argc, char *argv[])
 {
+	struct sigaction action{};
+	sigfillset(&action.sa_mask);
+	action.sa_handler = signal_handler;
+	sigaction(SIGINT, &action, nullptr);
+	
 	nandroid::DeviceTracker tracker;
-	tracker.update_connected_devices();
+	try
+	{
+		while(!quit)
+		{
+			tracker.update_connected_devices();
+			std::this_thread::sleep_for(std::chrono::seconds(2));
+		}
+	} 
+	catch(const std::exception& e)
+	{
+		nandroid::Logger::error("Caught exception!\n\t{}", e.what());
+	}
+	catch(...)
+	{
+		nandroid::Logger::error("Caught exception with no what() method!");
+	}
+
+	return 0;
 }
